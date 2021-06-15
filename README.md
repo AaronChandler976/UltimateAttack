4e4ccfd05b44d035ee7d6f8dbc08c3f24cc9f5a5
RusselBrouts <aaronaaron976@gmail.com>
2021-06-14 17:10:13 -0700

    first version

---
+++ README.md
@@ -0,0 +1,3 @@
+# UltimateAttack
+
+Simple name generator for your ultimate attack! Created for a portfolio project as part of Codecademy's Full-Stack Engineer course.
---
+++ ultimateAttack.js
@@ -0,0 +1,64 @@
+// Generate randum number between 0 and arrLength
+function randNum(arrLength) {
+    let num = Math.floor(Math.random() * arrLength);
+    return num;
+}
+
+// Create word banks for different parts of speech
+const attackNoun = ['arrow',   'assault', 'barrage', 'beam', 'bite', 'blade',
+    'blast',   'blow', 'cannon', 'claw', 'fang', 'fist', 'flame', 'gale', 'hammer',
+    'inferno', 'laser',   'might', 'palm', 'roar', 'slam', 'slash', 'smash', 'strike',
+    'sword', 'tempest', 'torrent', 'volley', 'whip'];
+
+const attackAdj = ['blazing', 'crushing', 'cutting', 'decimating', 'demonic', 'devouring',
+    'echoing', 'freezing', 'furious', 'holy', 'masterful', 'monstrous', 'radiant', 'raging', 
+    'ravenous', 'rending', 'roaring', 'sundering', 'thundering', 'twisting', 'unyielding', 
+    'vanishing', 'vicious'];
+
+const possessiveNoun = ["Emporer's", "God's", "King's", "berserker's", "dragon's", "heaven's",
+    "lightning's", "lion's", "mantis'", "master's", "mountain's", "slayer's", "titan's", 
+    "winter's"];
+
+const objectAdj = ['moonlit', 'infinite', 'whispering', 'misty', 'flowing', 'ashen', 'dying'];
+
+const objectNoun = ['beast', 'lake', 'mind', 'star', 'storm', 'vortex'];
+
+// Generate attack phrase
+function generateAttack() {
+    // Generate random words from each word bank
+    const aNoun1 = attackNoun[randNum(attackNoun.length)];
+    const aNoun2 = attackNoun[randNum(attackNoun.length)];
+    const aAdj1 = attackAdj[randNum(attackAdj.length)];
+    const aAdj2 = attackAdj[randNum(attackAdj.length)];
+    const pNoun = possessiveNoun[randNum(possessiveNoun.length)];
+    const oAdj = objectAdj[randNum(objectAdj.length)];
+    const oNoun = objectNoun[randNum(objectNoun.length)];
+    // Check for duplicate words
+    while (aNoun1 === aNoun2) {
+        aNoun2 = attackNoun[randNum(attackNoun.length)];
+    }
+    while (aAdj1 === aAdj2) {
+        aAdj2 = attackAdj[randNum(attackAdj.length)];
+    }
+    // Randomly determine phrase structure
+    const phraseIdx = randNum(5);
+    switch (phraseIdx) {
+        case 0:
+            return `${pNoun} ${aAdj1} ${aNoun1}!`;
+        case 1:
+            return `${oAdj} ${pNoun} ${aAdj1} ${aNoun1}!`;
+        case 2: 
+            return `${oAdj} ${oNoun}, ${aAdj1} ${aNoun1}!`;
+        case 3:
+            return `${aAdj1} ${aNoun1}, ${aAdj2} ${aNoun2}!`;
+        case 4:
+            return `${pNoun} ${aNoun1}, ${aAdj1} ${aNoun2}!`;
+    }
+}
+
+// Capitalize and print attack phrase
+function printAttack(attack) {
+    console.log(attack.toUpperCase());
+}
+
+printAttack(generateAttack());
